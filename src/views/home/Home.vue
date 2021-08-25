059637d4355051906184ebac92a7487e5bf5b9c6
Wang <linkwang@deloitte.com.cn>
2021-08-25 14:40:45 +0800

    更新首页内容

---
+++ src/views/home/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div> </nav-bar>
+    <home-swiper :banners="banners"r></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+import {getHomeMultidata} from "@/network/home";
+
+export default {
+  name: "Home",
+  data(){
+    return{
+      banners:[],
+      recommends:[]
+    }
+  },
+  components:{
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,36 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import Swiper from "@/components/common/swiper/Swiper";
+import SwiperItem from "@/components/common/swiper/SwiperItem";
+// import {Swiper, SwiperItem} from "@/components/common/swiper/Swiper"
+
+
+export default {
+  name: "HomeSwiper",
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return[]
+      }
+    }
+  },
+  components:{
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
