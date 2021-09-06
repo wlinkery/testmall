bab1cd5a2a04aa32a5fe542a661c5af26e1f63f5
Wang <linkwang@deloitte.com.cn>
2021-09-06 14:25:18 +0800

    小修改

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -28,46 +28,26 @@
       <li>libiao20</li>
     </ul>
     <ul>
-      <li>ll1</li>
-      <li>ll2</li>
-      <li>ll3</li>
-      <li>ll4</li>
-      <li>ll5</li>
-      <li>ll6</li>
-      <li>ll7</li>
-      <li>ll8</li>
-      <li>ll9</li>
-      <li>ll10</li>
-      <li>ll11</li>
-      <li>ll12</li>
-      <li>ll13</li>
-      <li>ll14</li>
-      <li>ll15</li>
-      <li>ll16</li>
-      <li>ll17</li>
-      <li>ll18</li>
-      <li>ll19</li>
-      <li>ll20</li>
-      <li>ll21</li>
-      <li>ll22</li>
-      <li>ll23</li>
-      <li>ll24</li>
-      <li>ll25</li>
-      <li>ll26</li>
-      <li>ll27</li>
-      <li>ll28</li>
-      <li>ll29</li>
-      <li>ll30</li>
-      <li>ll31</li>
-      <li>ll32</li>
-      <li>ll33</li>
-      <li>ll34</li>
-      <li>ll35</li>
-      <li>ll36</li>
-      <li>ll37</li>
-      <li>ll38</li>
-      <li>ll39</li>
-      <li>ll40</li>
+      <li>liebiao1</li>
+      <li>liebiao2</li>
+      <li>liebiao3</li>
+      <li>liebiao4</li>
+      <li>liebiao5</li>
+      <li>liebiao6</li>
+      <li>liebiao7</li>
+      <li>liebiao8</li>
+      <li>liebiao9</li>
+      <li>liebiao10</li>
+      <li>liebiao11</li>
+      <li>liebiao12</li>
+      <li>liebiao13</li>
+      <li>liebiao14</li>
+      <li>liebiao15</li>
+      <li>liebiao16</li>
+      <li>liebiao17</li>
+      <li>liebiao18</li>
+      <li>liebiao19</li>
+      <li>liebiao20</li>
     </ul>
 
   </div>
